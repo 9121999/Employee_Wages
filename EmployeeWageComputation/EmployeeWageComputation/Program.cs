261ba6e3ccc34690b56cbe4be89164502899707d
Prerana <dahiwadeprerana@gmail.com>
2022-11-11 22:16:41 +0530

    [prerana]Add:GreetingMsg

---
+++ EmployeeWageComputation/EmployeeWageComputation/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Employee_Wage
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employeewage = new Employee();
+            employeewage.Attendance();
+        }
+    }
+}
